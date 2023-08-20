55b9a42d8d919c1b0c352e2fd65c28879260a9e5
Dennis Hammerschlag <hmmden001@myuct.ac.za>
2023-08-20 17:32:31 +0200

    Moved dennis's scripts

--- HPC_scripts_Dennis/gpuDennis.sh
+++ HPC_scripts_Dennis/gpuDennis.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=a100free
 #SBATCH --partition=a100
 #SBATCH --nodes=1 --ntasks=2 --gres=gpu:a100-2g-10gb:1
-#SBATCH --time=20:00
+#SBATCH --time=50:00:00
 #SBATCH --job-name="ASTGCNJob"
 CUDA_VISIBLE_DEVICES=$(ncvd)
 module load python/miniconda3-py38-usr-A
