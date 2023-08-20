eb6d88364f261f09dc8346dbfaecbea095e8c856
Dennis Hammerschlag <hmmden001@myuct.ac.za>
2023-08-20 17:05:28 +0200

    Ran hpo for astgcn

---
+++ HPC_scripts_Dennis/gpuDennis.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --account=a100free
+#SBATCH --partition=a100
+#SBATCH --nodes=1 --ntasks=2 --gres=gpu:a100-2g-10gb:1
+#SBATCH --time=20:00
+#SBATCH --job-name="ASTGCNJob"
+CUDA_VISIBLE_DEVICES=$(ncvd)
+module load python/miniconda3-py38-usr-A
+source activate stgnnEnv
+python3 runASTGCN.py
