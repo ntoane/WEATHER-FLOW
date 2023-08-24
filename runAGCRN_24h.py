5c4b57c269a90a9566ae6ea6d36643382f544d38
adeebgaibie <101038476+adeebgaibie@users.noreply.github.com>
2023-08-24 20:56:54 +0200

    split agcrn into horizons

---
+++ runAGCRN_24h.py
@@ -0,0 +1,25 @@
+# from Execute.agcrnExecute import train
+import yaml
+import Logs.Evaluation as Evaluation
+from Execute.agcrnExecute import agcrnExecute
+from HPO.agcrnHPO import agcrnHPO
+
+###########################################################
+
+with open('configurations/agcrnConfig.yaml', 'r') as file:
+    agcrnConfig = yaml.safe_load(file)
+with open('configurations/sharedConfig_24h.yaml', 'r') as file:
+    sharedConfig = yaml.safe_load(file)
+
+
+
+
+# train(agcrnConfig, agcrnConfig)
+agcrn_trainer = agcrnExecute(sharedConfig, agcrnConfig)
+agcrn_trainer.execute() 
+
+# agcrn_hpo = agcrnHPO(sharedConfig, agcrnConfig)
+# agcrn_hpo.hpo() 
+
+Evaluation.AgcrnEval(agcrnConfig, sharedConfig)
+    
