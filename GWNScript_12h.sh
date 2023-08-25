b0e699e0e6e5198f483f2ec79c3a4e25595af66c
Dennis Hammerschlag <hmmden001@myuct.ac.za>
2023-08-25 11:35:51 +0200

    Fixed gwn script

--- GWNScript_12h.sh
+++ GWNScript_12h.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --account compsci
 #SBATCH --partition=ada
-#SBATCH --time=30:00:00
+#SBATCH --time=20:00:00
 #SBATCH --nodes=1 --ntasks=8
 #SBATCH --job-name="12h_GWN_Train"
 #SBATCH --mail-user=hmmden001@uct.ac.za
